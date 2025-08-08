fda588467c120c7040e0b9a1fc5ea0c7db3d3883
hush <mikhail@kenmoredesign.com>
2025-08-08 21:58:40 +0000

    cors

--- src/main.ts
+++ src/main.ts
@@ -12,10 +12,13 @@ import { AuthGuard } from './modules/auth/auth.guard';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-    app.enableCors({
-        origin: 'http://localhost:4200', // строго твой фронт!
-        credentials: true,
-    });
+  app.enableCors({
+    origin: [
+      'http://localhost:4200',
+      'https://auto-client-production.up.railway.app',
+    ],
+    credentials: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
